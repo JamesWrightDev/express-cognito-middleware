d51004c39ca9bc9db0048a8b563fdac24504b609
James <jameswrightdev@gmail.co.uk>
2021-07-27 20:36:30 +0100

    init

---
+++ dist/foo.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sayHello() {
+    console.log("hi");
+}
+exports.sayHello = sayHello;
---
+++ lib/foo.ts
@@ -0,0 +1,3 @@
+const veriify = () => {
+  console.log("hi");
+};
